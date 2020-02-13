ed70bce54d8ee4e5df7e7f4676208e6e935b2cfc
Paul92 <paul.gafton@gmail.com>
2020-02-13 16:40:18 +0100

    Cleanup

--- main.cpp
+++ main.cpp
@@ -36,37 +36,57 @@ Eigen::MatrixXd readDense(const std::string &filename) {
     return mat;
 }
 
-std::array<Eigen::ArrayXXd, 20> load_data() {
-    const auto eq130 = readDense("test_data/eq130_rug.txt");
-    const auto eq121 = readDense("test_data/eq121_rug.txt");
-    const auto eq112 = readDense("test_data/eq112_rug.txt");
-    const auto eq103 = readDense("test_data/eq103_rug.txt");
-    const auto eq120 = readDense("test_data/eq120_rug.txt");
-    const auto eq111 = readDense("test_data/eq111_rug.txt");
-    const auto eq102 = readDense("test_data/eq102_rug.txt");
-    const auto eq110 = readDense("test_data/eq110_rug.txt");
-    const auto eq101 = readDense("test_data/eq101_rug.txt");
-    const auto eq100 = readDense("test_data/eq100_rug.txt");
-    const auto eq230 = readDense("test_data/eq230_rug.txt");
-    const auto eq221 = readDense("test_data/eq221_rug.txt");
-    const auto eq212 = readDense("test_data/eq212_rug.txt");
-    const auto eq203 = readDense("test_data/eq203_rug.txt");
-    const auto eq220 = readDense("test_data/eq220_rug.txt");
-    const auto eq211 = readDense("test_data/eq211_rug.txt");
-    const auto eq202 = readDense("test_data/eq202_rug.txt");
-    const auto eq210 = readDense("test_data/eq210_rug.txt");
-    const auto eq201 = readDense("test_data/eq201_rug.txt");
-    const auto eq200 = readDense("test_data/eq200_rug.txt");
+Eigen::MatrixXd load_data() {
 
+    const Eigen::MatrixXd eq130 = readDense("test_data/eq130_rug.txt").col(0);
+    const Eigen::MatrixXd eq121 = readDense("test_data/eq121_rug.txt").col(0);
+    const Eigen::MatrixXd eq112 = readDense("test_data/eq112_rug.txt").col(0);
+    const Eigen::MatrixXd eq103 = readDense("test_data/eq103_rug.txt").col(0);
+    const Eigen::MatrixXd eq120 = readDense("test_data/eq120_rug.txt").col(0);
+    const Eigen::MatrixXd eq111 = readDense("test_data/eq111_rug.txt").col(0);
+    const Eigen::MatrixXd eq102 = readDense("test_data/eq102_rug.txt").col(0);
+    const Eigen::MatrixXd eq110 = readDense("test_data/eq110_rug.txt").col(0);
+    const Eigen::MatrixXd eq101 = readDense("test_data/eq101_rug.txt").col(0);
+    const Eigen::MatrixXd eq100 = readDense("test_data/eq100_rug.txt").col(0);
+    const Eigen::MatrixXd eq230 = readDense("test_data/eq230_rug.txt").col(0);
+    const Eigen::MatrixXd eq221 = readDense("test_data/eq221_rug.txt").col(0);
+    const Eigen::MatrixXd eq212 = readDense("test_data/eq212_rug.txt").col(0);
+    const Eigen::MatrixXd eq203 = readDense("test_data/eq203_rug.txt").col(0);
+    const Eigen::MatrixXd eq220 = readDense("test_data/eq220_rug.txt").col(0);
+    const Eigen::MatrixXd eq211 = readDense("test_data/eq211_rug.txt").col(0);
+    const Eigen::MatrixXd eq202 = readDense("test_data/eq202_rug.txt").col(0);
+    const Eigen::MatrixXd eq210 = readDense("test_data/eq210_rug.txt").col(0);
+    const Eigen::MatrixXd eq201 = readDense("test_data/eq201_rug.txt").col(0);
+    const Eigen::MatrixXd eq200 = readDense("test_data/eq200_rug.txt").col(0);
 
-    std::array<Eigen::ArrayXXd, 20> coeffs = {eq130, eq121, eq112, eq103, eq120, eq111, eq102, eq110, eq101, eq100,
-                                              eq230, eq221, eq212, eq203, eq220, eq211, eq202, eq210, eq201, eq200};
-    return coeffs;
+    Eigen::MatrixXd pol_coeff(eq130.rows(), 20);
+
+    pol_coeff.col(0)  = eq130;
+    pol_coeff.col(1 ) = eq121;
+    pol_coeff.col(2 ) = eq112;
+    pol_coeff.col(3 ) = eq103;
+    pol_coeff.col(4 ) = eq120;
+    pol_coeff.col(5 ) = eq111;
+    pol_coeff.col(6 ) = eq102;
+    pol_coeff.col(7 ) = eq110;
+    pol_coeff.col(8 ) = eq101;
+    pol_coeff.col(9 ) = eq100;
+    pol_coeff.col(10) = eq230;
+    pol_coeff.col(11) = eq221;
+    pol_coeff.col(12) = eq212;
+    pol_coeff.col(13) = eq203;
+    pol_coeff.col(14) = eq220;
+    pol_coeff.col(15) = eq211;
+    pol_coeff.col(16) = eq202;
+    pol_coeff.col(17) = eq210;
+    pol_coeff.col(18) = eq201;
+    pol_coeff.col(19) = eq200;
+
+    return pol_coeff;
 }
 
 template<typename T>
-void evaluate_polynomial(const Eigen::MatrixXd &coeffs, T k1, T k2,
-                         T *residuals) {
+void evaluate_polynomial(const Eigen::MatrixXd &coeffs, T k1, T k2, T *residuals) {
 
     T k1_pow[] = {T(1), k1, k1*k1, k1*k1*k1};
     T k2_pow[] = {T(1), k2, k2*k2, k2*k2*k2};
@@ -90,34 +110,26 @@ void evaluate_polynomial(const Eigen::MatrixXd &coeffs, T k1, T k2,
 class MyCostFunctor {
 public:
 
-    explicit MyCostFunctor(const std::array<Eigen::ArrayXXd, 20> &coeff, int polynomial) : coeff(coeff), polynomial(polynomial) { }
+    explicit MyCostFunctor(const Eigen::MatrixXd &pol_coeff) : pol_coeff(pol_coeff) { }
 
     template<typename T>
     bool operator()(const T* const k, T* residuals) const {
-
-        Eigen::MatrixXd pol_coeff(coeff[0].rows(), 20);
-
-        for (std::size_t i = 0; i < coeff.size(); i++)
-            pol_coeff.col(i) = coeff[i].col(polynomial);
-
         evaluate_polynomial<T>(pol_coeff.topRows(10), k[0], k[1], residuals);
-
         return true;
     }
 
 private:
-    const std::array<Eigen::ArrayXXd, 20> &coeff;
-    const int polynomial;
+    const Eigen::MatrixXd pol_coeff;
 };
 
 
-Eigen::ArrayXXd solve_polynomial(const std::array<Eigen::ArrayXXd, 20> &coeff) {
+Eigen::ArrayXXd solve_polynomial(const Eigen::MatrixXd &coeff) {
 
-    Eigen::ArrayXXd ks(2, coeff[0].cols());
+    Eigen::ArrayXXd ks(2, 1);
 
     ks = 0;
 
-    ceres::CostFunction *cost_function = new ceres::AutoDiffCostFunction<MyCostFunctor, 18, 2>(new MyCostFunctor(coeff, 0));
+    ceres::CostFunction *cost_function = new ceres::AutoDiffCostFunction<MyCostFunctor, 18, 2>(new MyCostFunctor(coeff));
 
     // Build the problem.
     ceres::Problem problem;
@@ -126,7 +138,8 @@ Eigen::ArrayXXd solve_polynomial(const std::array<Eigen::ArrayXXd, 20> &coeff) {
     ceres::Solver::Options options;
 
     options.minimizer_progress_to_stdout = true;
-    options.sparse_linear_algebra_library_type = ceres::SUITE_SPARSE;
+    options.sparse_linear_algebra_library_type = ceres::EIGEN_SPARSE;
+    options.linear_solver_type = ceres::DENSE_QR;
     options.num_threads = 8;
 
     ceres::Solver::Summary summary;
@@ -141,9 +154,9 @@ Eigen::ArrayXXd solve_polynomial(const std::array<Eigen::ArrayXXd, 20> &coeff) {
 
 int main() {
 
-    std::array<Eigen::ArrayXXd, 20> coeffs = load_data();
+    const auto pol_coeffs = load_data();
 
-    Eigen::ArrayXXd res = solve_polynomial(coeffs).transpose();
+    Eigen::ArrayXXd res = solve_polynomial(pol_coeffs).transpose();
 
     std::cout << res << std::endl;
 
